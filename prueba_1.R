6506790663fa72f2bbbf30a42892a3b3146ec2bc
Nelson Javier  Romero Yepez <nelsonry@TTC.CAT>
2023-11-17 13:01:05 +0100

    Cambio de algoritmo

--- prueba_1.R
+++ prueba_1.R
@@ -1,5 +1,11 @@
-df <- read.table("epa-http/epa-http.csv",sep = " ", quote = '"', fill = TRUE)
+#df <- read.table("epa-http/epa-http.csv",sep = " ", quote = '"', fill = TRUE)
 
-colnames(df) <-c("host", "timestamp", "request", "status", "bytes")
+#colnames(df) <-c("host", "timestamp", "request", "status", "bytes")
 
-          
+
+library(readr)
+epa_http <- read_table("epa-http/epa-http.csv", col_names = FALSE)
+
+colnames(epa_http) <-c("host", "timestamp", "method", "path", "http_ver", "status", "bytes")
+
+View(epa_http)
