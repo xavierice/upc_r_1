67c94c766946dc85a6843090bc8eac1540dc9241
Nelson Javier  Romero Yepez <nelsonry@TTC.CAT>
2023-11-17 16:02:55 +0100

    Limpieza terminada 1

--- prueba_1.R
+++ prueba_1.R
@@ -1,24 +1,15 @@
-epa_http <- read.table("epa-http/epa-http.csv",sep = " ", quote = '"', fill = TRUE)
-
-colnames(epa_http) <-c("host", "timestamp", "request", "status", "bytes")
-
 #Carga de CSV 
-#library(readr)
-#epa_http <- read_table("epa-http/epa-http.csv", col_names = FALSE, col_types = NULL)
-#asignacion de headers
-#colnames(epa_http) <-c("host", "timestamp", "method", "path", "http_ver", "status", "bytes")
+epa_http <- read.table("epa-http/epa-http.csv",sep = " ", quote = '"', fill = TRUE)
+colnames(epa_http) <-c("host", "timestamp", "request", "status", "bytes")
 
 library(stringr)
 library(data.table)
 
 #limpieza de datos
-#epa_http$method <- str_replace(epa_http$method, "\"", "")
-#epa_http$http_ver <- str_replace(epa_http$http_ver, "\"", "")
-
-#Cambio de string a datetime
 epa_http$timestamp <- as.POSIXct(epa_http$timestamp, format="[%d:%H:%M:%S]", tz="UTC")
 
-epa_http <- tidyr::separate(epa_http, request, c("method", "path", "http_ver"), sep = " ")
+#parceo de datos
+epa_http <- tidyr::separate(epa_http, request, c("tipo", "url", "protocolo"), sep = " ")
 
 View(epa_http)
 
