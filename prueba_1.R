5d29e44f93174517aac9662a02db6073d03a72b9
Nelson Javier  Romero Yepez <nelsonry@TTC.CAT>
2023-11-17 15:58:38 +0100

    Limpieza terminada

--- prueba_1.R
+++ prueba_1.R
@@ -1,11 +1,24 @@
-#df <- read.table("epa-http/epa-http.csv",sep = " ", quote = '"', fill = TRUE)
+epa_http <- read.table("epa-http/epa-http.csv",sep = " ", quote = '"', fill = TRUE)
 
-#colnames(df) <-c("host", "timestamp", "request", "status", "bytes")
+colnames(epa_http) <-c("host", "timestamp", "request", "status", "bytes")
 
+#Carga de CSV 
+#library(readr)
+#epa_http <- read_table("epa-http/epa-http.csv", col_names = FALSE, col_types = NULL)
+#asignacion de headers
+#colnames(epa_http) <-c("host", "timestamp", "method", "path", "http_ver", "status", "bytes")
 
-library(readr)
-epa_http <- read_table("epa-http/epa-http.csv", col_names = FALSE)
+library(stringr)
+library(data.table)
 
-colnames(epa_http) <-c("host", "timestamp", "method", "path", "http_ver", "status", "bytes")
+#limpieza de datos
+#epa_http$method <- str_replace(epa_http$method, "\"", "")
+#epa_http$http_ver <- str_replace(epa_http$http_ver, "\"", "")
+
+#Cambio de string a datetime
+epa_http$timestamp <- as.POSIXct(epa_http$timestamp, format="[%d:%H:%M:%S]", tz="UTC")
+
+epa_http <- tidyr::separate(epa_http, request, c("method", "path", "http_ver"), sep = " ")
 
 View(epa_http)
+
